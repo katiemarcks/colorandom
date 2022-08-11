ccd78121677bba0a95da7e01b934ec6b28337287
Katie Marcks <katiemarcks7@gmail.com>
2022-08-11 14:51:33 -0600

    Add hex codes to saved palettes section

--- saved-palettes.js
+++ saved-palettes.js
@@ -15,23 +15,40 @@ function renderSavedPalettes() {
   for (let i = 0; i < savedPalettes.length; i++) {
     const element = savedPalettes[i];
     savedPalettesSection.innerHTML += `
-    <div style="display: flex; justify-content: space-around;" > 
+    <div style="display: flex; justify-content: space-around; gap: 10px; margin: 10px;" > 
+    
+    <div>
     <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${element.colors[0].color}">
-    
     </div>
+    <p style="font-size: 10px;">${element.colors[0].color}</p>
+    </div>
+
+    <div>
     <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${element.colors[1].color}">
-    
     </div>
+    <p style="font-size: 10px;">${element.colors[1].color}</p>
+    </div>
+
+    <div>
     <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${element.colors[2].color}">
-    
     </div>
+    <p style="font-size: 10px;">${element.colors[2].color}</p>
+    </div>
+
+    <div>
     <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${element.colors[3].color}">
-    
     </div>
+    <p style="font-size: 10px;">${element.colors[3].color}</p>
+    </div>
+
+    <div>
     <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${element.colors[4].color}">
-    
     </div>
-    <img id="${element.id}" onClick="deleteSavedPalette(event)" style="height:17px; width:17px" src="assets/images/delete.png">
+    <p style="font-size: 10px;">${element.colors[4].color}</p>
+    </div>
+
+
+    <img id="${element.id}" onClick="deleteSavedPalette(event)" style="height:17px; width:17px; cursor: pointer" src="assets/images/delete.png">
     </div>
 
     `;
